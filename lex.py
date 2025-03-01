eeda29d725c620469e76f12773f1937f3cd620b4
Noah Scott <149430676+BareMetal-Alchemist@users.noreply.github.com>
2025-02-28 17:14:12 -0800

    Add files via upload
    Added lexer to dfa

--- lex.py
+++ lex.py
@@ -1,37 +1,190 @@
-class DFA:
-    def init(self, states, alphabet, transitions, start_state, accept_states):
-        self.states = states
-        self.alphabet = alphabet
-        self.transitions = transitions
-        self.start_state = start_state
-        self.accept_states = accept_states
 
-    def is_accepting(self, state):
-        return state in self.accept_states
+class Dfa:
+    def __init__(self, sts, alph, trans, start, accStates):
+        self.sts = sts
+        self.alph = alph
+        self.trans = trans
+        self.start = start
+        self.accStates = accStates
 
-    def process_string(self, input_string):
-        current_state = self.start_state
-        for symbol in input_string:
-            if symbol not in self.alphabet:
-                raise ValueError(f"Symbol '(symbol)' is not in the alphabet")
-            current_state = self.transitions.get((current_state, symbol))
-            if current_state is None:
-                raise ValueError(f"No transition deined for state '{current_state}'")
-        return self.is_accepting(current_state)
+    def procStr(self, inpStr):
+        curState = self.start
+        lex = ""
+        lastAccState = None
+        lastLex = ""
+        
+        for sym in inpStr:
+            if sym not in self.alph:
+                break  
+            nxtState = self.trans.get((curState, sym))
+            if nxtState is None:
+                break  
+            curState = nxtState
+            lex += sym
+            if curState in self.accStates:
+                lastAccState = curState
+                lastLex = lex
+        
+        if lastAccState is None:
+            raise ValueError("No valid token found in input")
+        
+        tokType = self.accStates[lastAccState]
+        return tokType, lastLex
 
-states = {'q0', 'q1', 'q2'}
-alphabet = {'1', '0'}
 
-transitions = {
-        ('q0', '1') : 'q1',
-        ('q0', '0') : 'q0',
-        ('q1', '1') : 'q2',
-        ('q1', '0'): 'q2',
-        ('q2' , '1') : 'q2',
-        ('q2', '1') : 'q2'
-}
+# Dfa for ids (matches a letter followed by letters/digits)
+idSts = {"start", "inId"}
+idAlph = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+idTrans = {}
+for letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    idTrans[("start", letter)] = "inId"
+for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789":
+    idTrans[("inId", char)] = "inId"
+idStart = "start"
+idAccStates = {"inId": "id"}
+idDfa = Dfa(idSts, idAlph, idTrans, idStart, idAccStates)
 
-start_state = 'q0'
-accept_state = {'q2'}
 
-dfa = DFA(states, alphabet, transitions, start_state, accept_states)
+# Dfa for nums (matches one or more digits)
+numSts = {"start", "inNum"}
+numAlph = set("0123456789")
+numTrans = {}
+for digit in "0123456789":
+    numTrans[("start", digit)] = "inNum"
+for digit in "0123456789":
+    numTrans[("inNum", digit)] = "inNum"
+numStart = "start"
+numAccStates = {"inNum": "num"}
+numDfa = Dfa(numSts, numAlph, numTrans, numStart, numAccStates)
+
+
+# Lexer class using DFAs and table-driven approach for opperators and keywords
+class Lexer:
+    def __init__(self, src):
+        self.src = src
+        self.pos = 0
+        self.toks = []
+        # Keywords per project reqs
+        self.kws = {"int", "return", "if", "switch", "float", "while", 
+                    "else", "case", "char", "for", "goto", "unsigned", 
+                    "main", "break", "continue", "void"}
+
+    def nextChar(self):
+        if self.pos < len(self.src):
+            ch = self.src[self.pos]
+            self.pos += 1
+            return ch
+        return None
+
+    def peekChar(self):
+        if self.pos < len(self.src):
+            return self.src[self.pos]
+        return None
+
+    def lex(self):
+        while self.pos < len(self.src):
+            ch = self.peekChar()
+            if ch.isspace():
+                self.pos += 1
+                continue
+            # Handle comments: skip '//' until newline
+            if ch == '/' and self.pos + 1 < len(self.src) and self.src[self.pos+1] == '/':
+                self.lexComment()
+                continue
+            if ch.isalpha():
+                tok, lex = self.lexId()
+                if lex in self.kws:
+                    tok = lex  # Token is the lexeme for keywords
+                self.toks.append((tok, lex))
+                continue
+            if ch.isdigit():
+                tok, lex = self.lexNum()
+                self.toks.append((tok, lex))
+                continue
+            tok, lex = self.lexOpSep()
+            self.toks.append((tok, lex))
+        return self.toks
+
+    def lexId(self):
+        substr = ""
+        while self.pos < len(self.src) and self.src[self.pos].isalnum():
+            substr += self.src[self.pos]
+            self.pos += 1
+        tok, lex = idDfa.procStr(substr)
+        return tok, lex
+
+    def lexNum(self):
+        substr = ""
+        while self.pos < len(self.src) and self.src[self.pos].isdigit():
+            substr += self.src[self.pos]
+            self.pos += 1
+        tok, lex = numDfa.procStr(substr)
+        return tok, lex
+
+    def lexOpSep(self):
+        ch = self.nextChar()
+        nxt = self.peekChar() if self.peekChar() is not None else ''
+        # Check multi-char tokens first
+        if ch == '+' and nxt == '+':
+            self.pos += 1
+            return ("incr", "++")
+        if ch == '-' and nxt == '-':
+            self.pos += 1
+            return ("decr", "--")
+        if ch == '<' and nxt == '=':
+            self.pos += 1
+            return ("leq", "<=")
+        if ch == '>' and nxt == '=':
+            self.pos += 1
+            return ("geq", ">=")
+        if ch == '=' and nxt == '=':
+            self.pos += 1
+            return ("eq", "==")
+        if ch == '&' and nxt == '&':
+            self.pos += 1
+            return ("land", "&&")
+        if ch == '|' and nxt == '|':
+            self.pos += 1
+            return ("lor", "||")
+        
+        # Single-char tokens mapped in a dict
+        tokMap = {
+            '(': "lParen",
+            ')': "rParen",
+            '[': "lBrack",
+            ']': "rBrack",
+            '{': "lBrace",
+            '}': "rBrace",
+            '.': "dot",
+            '+': "plus",
+            '-': "minus",
+            '*': "mult",
+            '/': "div",
+            '%': "mod",
+            '<': "lt",
+            '>': "gt",
+            '=': "assign",
+            ';': "semi",
+            ',': "comma",
+            '!': "lnot",
+            '&': "band",
+            '|': "bor"
+        }
+        tok = tokMap.get(ch, "unk")
+        return (tok, ch)
+
+    def lexComment(self):
+        self.pos += 2  # skip '//'
+        while self.pos < len(self.src) and self.src[self.pos] != '\n':
+            self.pos += 1
+        if self.pos < len(self.src) and self.src[self.pos] == '\n':
+            self.pos += 1  # skip newline
+
+
+# Example usage:
+if __name__ == "__main__":
+    srcCode = "int x = 10; // init x"
+    lexr = Lexer(srcCode)
+    tokens = lexr.lex()
+    for tok in tokens:
+        print(tok)
